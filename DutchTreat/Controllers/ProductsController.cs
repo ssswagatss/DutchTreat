4517ef1e66bc57554c28419e3e87e2c8d97bf003
Swagat Swain <swagat.swain@mindfiresolutions.com>
2018-07-31 19:07:48 +0530

    Adds the first api controller

---
+++ DutchTreat/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DutchTreat.Data;
+using DutchTreat.Data.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DutchTreat.Controllers
+{
+    //[Produces("application/json")]
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller
+    {
+
+        private readonly IDutchRepository _repository;
+        public readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(IDutchRepository repository, ILogger<ProductsController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_repository.GetProducts());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"An Error occurred {ex}");
+                return BadRequest("Not Found");
+            }
+        }
+    }
+}
