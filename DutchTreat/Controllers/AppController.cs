8c4445fe0768dfff9c6f8d2ffbb9e3777ef59a7b
Swagat Swain <swagat.swain@mindfiresolutions.com>
2018-07-31 13:05:47 +0530

    Layout and Tag Helpers

---
+++ DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Home";
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact";
+            return View();
+        }
+        public IActionResult About()
+        {
+            ViewBag.Title = "About";
+            return View();
+        }
+    }
+}
--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -15,6 +15,7 @@ namespace DutchTreat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +26,12 @@ namespace DutchTreat
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            //Serve static files
+            app
+                .UseStaticFiles()
+                .UseMvc(cfg=> {
+                    cfg.MapRoute("Default", "{controller}/{action}/{id?}", new { controller = "App", action = "Index" });
+                });
         }
     }
 }
